20a40d9d4382fba58b5b5ba6c99cd0ab05b9d457
LordeGeorge <george.ken.127@gmail.com>
2023-12-31 09:37:02 +0300

    api routes, auth controller, axios

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email'=>['required','email'],
+            'password'=>['required'],
+            'remember'=>'boolean',
+        ]);
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials,$remember)){
+            return response([
+                'message'=>'Incorrect Username or Password'
+            ],422);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        if (!$user->is_admin){
+            Auth::logout();
+            return response([
+                'message'=>'You dont have permission to Authenticate as Admin'
+            ],403);
+        }
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+    public function logout(){
+        /** @var User $user */
+        $user = User::user();
+        $user->currentAccessToken()->delete();
+        return response('',204);
+    }
+}
