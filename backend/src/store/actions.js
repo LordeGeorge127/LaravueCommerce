f340e2ff3ef802565bfb5d16e99fe8ec8905b075
LordeGeorge <george.ken.127@gmail.com>
2024-01-07 20:33:10 +0300

    Delete function

--- backend/src/store/actions.js
+++ backend/src/store/actions.js
@@ -7,28 +7,32 @@ import axiosClient from "../axios.js";
 //             return response;
 //         })
 // }
-export function getUser({commit},data){
-    return axiosClient.get('/user',data)
-        .then(({data})=>{
+export function getUser({commit}, data) {
+    return axiosClient.get('/user', data)
+        .then(({data}) => {
             // debugger;
-            commit('setUser',data)
+            commit('setUser', data)
             return data;
         })
 }
-export function login({commit},data) {
-    return axiosClient.post('/login',data)
-        .then(({data})=>{commit('setUser',data.user);
-            commit('setToken',data.token);
+
+export function login({commit}, data) {
+    return axiosClient.post('/login', data)
+        .then(({data}) => {
+            commit('setUser', data.user);
+            commit('setToken', data.token);
             return data;
         })
 }
-export function logout({commit}){
+
+export function logout({commit}) {
     return axiosClient.post('/logout')
-        .then((response)=>{
-            commit('setToken',null)
+        .then((response) => {
+            commit('setToken', null)
             return response;
         })
 }
+
 // export  function getProducts({commit}) {
 //     commit('setProducts',[true]);
 //     return axiosClient.get('product')
@@ -39,52 +43,56 @@ export function logout({commit}){
 //             commit('setProducts',[false])
 //         })
 // }
-export  function getProducts({commit},{url = null,search = '', perPage = 10,sort_field,sort_direction}) {
-    commit('setProducts',[true]);
+export function getProducts({commit}, {url = null, search = '', perPage = 10, sort_field, sort_direction} = {}) {
+    commit('setProducts', [true]);
     url = url || '/products';
-    return axiosClient.get(url,{
-     params :{
-         search,
-         per_page : perPage,
-         sort_field,
-         sort_direction
-     }
+    return axiosClient.get(url, {
+        params: {
+            search,
+            per_page: perPage,
+            sort_field,
+            sort_direction
+        }
     })
-        .then(res =>{
-            commit('setProducts',[false, res.data])
+        .then(res => {
+            commit('setProducts', [false, res.data])
         })
-        .catch(()=>{
-            commit('setProducts',[false])
+        .catch(() => {
+            commit('setProducts', [false])
         })
 }
 
-export function createProduct({commit},product){
+export function createProduct({commit}, product) {
     // debugger
-    if (product.image instanceof File){
+    if (product.image instanceof File) {
         let form = new FormData();
-        form.append('title',product.title);
-        form.append('image',product.image);
-        form.append('description',product.description);
-        form.append('price',product.price);
+        form.append('title', product.title);
+        form.append('image', product.image);
+        form.append('description', product.description);
+        form.append('price', product.price);
         product = form;
         // console.log(form)
     }
-    return axiosClient.post('/products',product)
+    return axiosClient.post('/products', product)
 }
 
-export function updateProduct({commit},product){
+export function updateProduct({commit}, product) {
     const id = product.id;
-    if (product.image instanceof File){
+    if (product.image instanceof File) {
         const form = new FormData();
-        form.append('id',product.id);
-        form.append('title',product.title);
-        form.append('image',product.image);
-        form.append('description',product.description);
-        form.append('price',product.price);
-        form.append('_method','PUT');
+        form.append('id', product.id);
+        form.append('title', product.title);
+        form.append('image', product.image);
+        form.append('description', product.description);
+        form.append('price', product.price);
+        form.append('_method', 'PUT');
         product = form;
-    }else{
+    } else {
         product._method = 'PUT';
     }
-    return axiosClient.post(`/products/${id}`,product );
+    return axiosClient.post(`/products/${id}`, product);
+}
+
+export function deleteProduct({commit}, id) {
+    return axiosClient.delete(`/products/${id}`)
 }
--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
+import {PencilIcon,TrashIcon,EllipsisVerticalIcon} from '@heroicons/vue/24/outline'
 import {computed, onMounted, ref} from "vue";
 import TableHeaderCell from "../../components/core/TableHeaderCell.vue";
 import Spinner from "../../components/core/Spinner.vue";
@@ -52,6 +54,15 @@ function getForPage(ev, link) {
         return;
     }
     getProducts(link.url)
+}
+function deleteProduct(product) {
+    if (!window.confirm('Are you sure you want to delete this product')){
+        return;
+    }
+    store.dispatch('deleteProduct',product.id)
+        .then(res=>{
+            store.dispatch('getProducts')
+        })
 }
 </script>
 
@@ -121,6 +132,67 @@ function getForPage(ev, link) {
                 </td>
                 <td class="border-b p-2">$ &nbsp;{{ product.price }}</td>
                 <td class="border-b p-2">{{ product.updated_at }}</td>
+                <td class="border-b p-2">
+                    <Menu as="div" class="relative inline-block text-left">
+                        <div>
+                            <MenuButton
+                                class="inline-flex justify-center rounded-md  text-sm font-medium text-white w-10 h-10 bg-black bg-opacity-0  focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                            >
+                                <EllipsisVerticalIcon
+                                    class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+                                    aria-hidden="true"
+                                />
+                            </MenuButton>
+                        </div>
+
+                        <transition
+                            enter-active-class="transition duration-100 ease-out"
+                            enter-from-class="transform scale-95 opacity-0"
+                            enter-to-class="transform scale-100 opacity-100"
+                            leave-active-class="transition duration-75 ease-in"
+                            leave-from-class="transform scale-100 opacity-100"
+                            leave-to-class="transform scale-95 opacity-0"
+                        >
+                            <MenuItems
+                                class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                            >
+                                <div class="px-1 py-1">
+                                    <MenuItem v-slot="{ active }">
+                                        <button
+                                            :class="[
+                                                      active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                                                      'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                    ]"
+                                        >
+                                            <PencilIcon
+                                                :active="active"
+                                                class="mr-2 h-5 w-5 text-violet-400"
+                                                aria-hidden="true"
+                                            />
+                                            Edit
+                                        </button>
+                                    </MenuItem>
+                                    <MenuItem v-slot="{ active }">
+                                        <button
+                                            :class="[
+                                              active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                                              'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                            ]"
+                                            @click="deleteProduct(product)"
+                                        >
+                                            <TrashIcon
+                                                :active="active"
+                                                class="mr-2 h-5 w-5 text-violet-400"
+                                                aria-hidden="true"
+                                            />
+                                            Delete
+                                        </button>
+                                    </MenuItem>
+                                </div>
+                            </MenuItems>
+                        </transition>
+                    </Menu>
+                </td>
 
             </tr>
             </tbody>
