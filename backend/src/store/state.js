5bc5ecf2def09dd46b6d031e52aa7d450ee466c9
LordeGeorge <george.ken.127@gmail.com>
2024-01-11 19:41:54 +0300

    seeder migration edits

--- backend/src/store/state.js
+++ backend/src/store/state.js
@@ -1,5 +1,4 @@
 import {createStore} from "vuex";
-import * as actions from './actions.js';
 import * as mutations from './mutations.js';
 
 const state = {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ProductSeeder::class,
+            AdminUserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
     }
