00a23675929ea2bc2fcbeb740c882fd5a2744e17
LordeGeorge <george.ken.127@gmail.com>
2024-01-12 19:44:01 +0300

    views for forgot p/w, login, register,reser-pwd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,74 @@
-<x-guest-layout>
+<x-app-layout>
+    <form
+        action="{{ route('register') }}"
+        method="post"
+        class="w-[400px] mx-auto p-6 my-16"
+        x-data="signupForm"
+        @submit.prevent="submit"
+    >
+        @csrf
+        <h2 class="text-2xl font-semibold text-center mb-4">Create an account</h2>
+        <p class="text-center text-gray-500 mb-3">
+            or
+            <a
+                href="{{ route('login') }}"
+                class="text-sm text-purple-700 hover:text-purple-600"
+            >login with existing account</a
+            >
+        </p>
+        <div class="mb-4">
+            <x-text-input
+                placeholder="Your name"
+                type="text"
+                name="name"
+                x-model="form.name"
+                @input="validateName()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+            />
+            <small x-show="errors.name" x-text="errors.name" class="text-red-600"></small>
+        </div>
+        </p>
+        <div class="mb-4">
+            <x-text-input
+                placeholder="Your Email"
+                type="email"
+                name="email"
+                x-model="form.email"
+                @input="validateEmail()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+            />
+            <small x-show="errors.email" x-text="errors.email" class="text-red-600"></small>
+        </div>
+        <div class="mb-4">
+            <x-text-input
+                placeholder="Password"
+                type="password"
+                name="password"
+                x-model="form.password"
+                @input="validatePassword()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+            />
+            <small x-show="errors.password" x-text="errors.password" class="text-red-600"></small>
+        </div>
+        </div>
+        <div class="mb-4">
+            <x-text-input
+                placeholder="Repeat Password"
+                type="password"
+                name="password_confirmation"
+                x-model="form.password_repeat"
+                @input="validatePasswordRepeat()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+            />
+            <small x-show="errors.password_repeat" x-text="errors.password_repeat" class="text-red-600"></small>
+        </div>
+
+        <button
+            class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 w-full"
+        >
+            Signup
+        </button>
+    </form>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +119,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-app-layout>
