99c8ef264f9c51a3512d5bbc81ac5268f16ac0e6
LordeGeorge <george.ken.127@gmail.com>
2024-01-13 19:45:45 +0300

    error validators

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,8 +1,9 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-white space-y-1 bg-red-600 px-4 py-3 text-center']) }}>
         @foreach ((array) $messages as $message)
+{{--            <li>Wrong Email or Password </li>--}}
             <li>{{ $message }}</li>
         @endforeach
     </ul>
