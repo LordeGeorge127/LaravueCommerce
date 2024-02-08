32c1cfcbf30b4375abe916bef3c5d789e25db378
LordeGeorge <george.ken.127@gmail.com>
2024-02-08 15:09:06 +0300

    mail sending when user registers

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-white bg-green-600 px-3 py-2']) }}>
         {{ $status }}
     </div>
 @endif
