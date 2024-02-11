f0a41b9505a59a5189f011462f8cf927e471c6fb
LordeGeorge <george.ken.127@gmail.com>
2024-02-11 12:52:00 +0300

    view product by slug in frontend

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,8 @@ class Product extends Model
             ->generateSlugsFrom('title')
             ->saveSlugsTo('slug');
     }
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
